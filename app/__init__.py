bc2a09ea0163ff6d8b09bd49887a4a5bea7549a9
Jiahui Xu <xujiahui219@gmail.com>
2024-04-12 21:43:14 -0700

    add os

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify, after_this_request, render_template, 
 from flask_cors import CORS
 import pandas as pd
 from flask_restful import Resource, Api
+import os
 
 app = Flask(__name__)
 from app import routes
